69812bfd64ecb0b76e25db91721beafae8f56c89
Jakub Tkacz <jtkacz@expo.io>
2025-07-07 17:55:33 +0200

    initial commit

---
+++ packages/expo-router/ios/Portal/ModalPortalHostView.swift
@@ -0,0 +1,70 @@
+import ExpoModulesCore
+
+class ModalPortalHostView: ExpoView {
+  var hostId: String = ""
+  var fluid: Bool = false
+  private var contentView: ModalPortalContentView?
+
+  let onRegistered = EventDispatcher()
+  let onUnregistered = EventDispatcher()
+
+  required init(appContext: AppContext? = nil) {
+    super.init(appContext: appContext)
+    clipsToBounds = true
+  }
+
+  func setHostId(hostId: String) {
+    if hostId == self.hostId {
+      return
+    }
+    PortalHostsRegistry.shared.unregister(hostId: self.hostId)
+    self.onUnregistered([
+      "hostId": self.hostId
+    ])
+    self.hostId = hostId
+    PortalHostsRegistry.shared.register(host: self)
+    self.onRegistered([
+      "hostId": self.hostId
+    ])
+  }
+
+  func setSize(width: Float, height: Float) {
+    if !fluid {
+      self.setShadowNodeSize(width, height: height)
+    }
+  }
+
+  override func removeFromSuperview() {
+    PortalHostsRegistry.shared.unregister(hostId: self.hostId)
+    self.onUnregistered([
+      "hostId": self.hostId
+    ])
+    super.removeFromSuperview()
+  }
+
+  func setContentView(contentView: ModalPortalContentView) {
+    if self.contentView != nil {
+      self.unmountContentView()
+    }
+    self.contentView = contentView
+    self.contentView?.setHostComponent(self)
+    if fluid {
+      self.contentView?.setSize(
+        width: Float(self.bounds.width), height: Float(self.bounds.height))
+    }
+    self.addSubview(contentView)
+  }
+
+  func unmountContentView() {
+    self.contentView?.removeFromSuperview()
+    self.contentView = nil
+  }
+
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    if fluid {
+      self.contentView?.setSize(
+        width: Float(self.bounds.width), height: Float(self.bounds.height))
+    }
+  }
+}
