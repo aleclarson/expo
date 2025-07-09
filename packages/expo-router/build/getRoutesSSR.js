69ebebb94e99768f46e0e4c5ac1895574591c4fe
Hassan Khan <hassan@expo.dev>
2025-07-09 13:09:19 +0100

    [router] allow static rewriting of routes
    Fixes ENG-16201

--- packages/expo-router/build/getRoutesSSR.js
+++ packages/expo-router/build/getRoutesSSR.js
@@ -18,7 +18,7 @@ const getRoutesCore_1 = require("./getRoutesCore");
  */
 function getRoutes(contextModule, options = {}) {
     return (0, getRoutesCore_1.getRoutes)(contextModule, {
-        getSystemRoute({ route, type, defaults }) {
+        getSystemRoute({ route, type, defaults, redirectConfig, rewriteConfig }) {
             if (route === '' && type === 'layout') {
                 // Root layout when no layout is defined.
                 return {
@@ -62,11 +62,21 @@ function getRoutes(contextModule, options = {}) {
                     children: [],
                 };
             }
-            else if ((type === 'redirect' || type === 'rewrite') && defaults) {
+            else if (type === 'redirect' && redirectConfig && defaults) {
                 return {
                     ...defaults,
                     loadRoute() {
-                        return require('./getRoutesRedirects').getRedirectModule(route);
+                        return require('./getRoutesRedirects').getRedirectModule(redirectConfig);
+                    },
+                };
+            }
+            else if (type === 'rewrite' && rewriteConfig && defaults) {
+                return {
+                    ...defaults,
+                    loadRoute() {
+                        return {
+                            default: contextModule(rewriteConfig.destinationContextKey).default,
+                        };
                     },
                 };
             }
